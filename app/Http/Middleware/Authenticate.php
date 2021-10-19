a679b3cf0ecb6eddc44b65c2588c697fa1e04939
Vinh217 <doquangvinhnt@gmail.com>
2021-10-20 01:21:47 +0700

    add condition user login

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -15,6 +15,10 @@ class Authenticate extends Middleware
     protected function redirectTo($request)
     {
         if (! $request->expectsJson()) {
+
+            if($request->routeIs('user.*')){
+                return route('user.login');
+            }
             return route('login');
         }
     }
